ae7c7a8de402e575d66241273577ac3bbe02494e
Andrei Bancos <bancos.andrei.2002@gmail.com>
2024-10-21 16:55:08 +0300

    Init front-end and back-end

---
+++ back-end/src/test/java/com/andreibancos/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreibancos.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
